38b3107fb780996b8c7ec20220259b48db91ebf7
DULA <balbal>
2022-05-19 20:37:05 +0300

    multiprocessing

--- time_series/Kaggle_Store_Sales_detrending.py
+++ time_series/Kaggle_Store_Sales_detrending.py
@@ -1,5 +1,6 @@
 # SARIMAX - From here
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import sm as sm
@@ -16,16 +17,22 @@ register_matplotlib_converters()
 sns.set_style("darkgrid")
 from statsmodels.tsa.seasonal import seasonal_decompose
 import statsmodels.api as sm
+import multiprocessing
 
 plt.rc("figure", figsize=(16, 12))
 plt.rc("font", size=13)
 
 #Copy of dataset
-Train = pd.read_csv('train.csv', parse_dates=["date"])
+Train = pd.read_csv('data/train.csv', parse_dates=["date"])
 Train = Train.set_index("date").to_period("D")
 
 stores = Train.store_nbr.unique()
 families = Train.family.unique()
+
+def getResultes(models):
+    for j in range(LEN):
+        results.append(models[j].fit(disp=False))
+        print(f'{j} / 1781')
 
 i = 0
 datasets = []
@@ -44,17 +51,62 @@ for store in stores:
         #print(X.sales, family)
         if X.sales.mean() > 2:
             #models_SARIMAX.append(sm.tsa.statespace.SARIMAX(X.sales, trend="c", order=(1,1,1), seasonal_order=(1, 0, 1, 7)))
-            models_SARIMAX.append(sm.tsa.statespace.SARIMAX(X.sales, order=(1,1,1), seasonal_order=(1, 0, 1, 7)))
+            models_SARIMAX.append(sm.tsa.statespace.SARIMAX(X.sales, trend="c", order=(1,1,1), seasonal_order=(1, 0, 1, 7)))
             #models_SARIMAX.append(sm.tsa.statespace.SARIMAX(X.sales, order=(1,1,1)))
             model_trained += 1
             datasets.append(X.sales)
 
-#Memory issues starts here:
+cores = 2
+j = 0
+for i in range(cores):
+    Processes = []
+    p = multiprocessing.Process(target=getResultes, args=models_SARIMAX[j:round(i*len(models_SARIMAX)/cores)])
+    j = j + len(models_SARIMAX)/cores
+    Processes.append(p)
+    p.start()
 
-for j in range(100):
-    #@timing
-    results.append(models_SARIMAX[j].fit(disp=False))
-    #print(f'{j} / 1781')
 
-results[2].plot_diagnostics(figsize=(16, 8))
+
+result = models_SARIMAX[239].fit(disp=False)
+#results[238].plot_diagnostics(figsize=(16, 8))
+result.plot_diagnostics(figsize=(16, 8))
 plt.show()
+
+k = 2
+start_forecast = 100
+pred = results[k].get_prediction(start=start_forecast, dynamic=False)
+pred_ci = pred.conf_int()
+
+ax = datasets[k].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='Predictions', alpha=.7)
+
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Sales')
+plt.legend()
+
+plt.show()
+
+#%% Forecast 16 days
+days = 16
+k=2
+#current_dir = 'C:\Users\DULA\PycharmProjects\ex4-signals'
+pred_uc = results[k].get_forecast(steps=1*days)
+pred_ci = pred_uc.conf_int()
+ax = datasets[k].plot(label='observed', figsize=(30, 7))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Sales')
+plt.legend()
+plt.show()
+#plt.savefig(current_dir + 'SARIMAX_FORECAST_1y.png')
+plt.close()
+
+print(results[k].summary())
+print(pred_uc.predicted_mean)
