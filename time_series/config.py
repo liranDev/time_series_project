c4f73be952811b8ce12695724e3583c643335095
liran shani <liranshani85@gmail.com>
2022-06-18 11:39:53 +0300

    WIP

--- time_series/config.py
+++ time_series/config.py
@@ -8,9 +8,13 @@ MODEL_DATA = Path.cwd().parent / 'model_data'
 TRAINED_MODELS_PATH = Path.cwd().parent / 'models'
 DATA_SETS_PATH = MODEL_DATA / 'data_sets.data'
 SARIMAX_MODELS_PATH = MODEL_DATA / 'srimax_models.data'
+RESULTS = Path.cwd().parent / 'results'
 
 if not Path(TRAINED_MODELS_PATH).is_dir():
     os.mkdir(TRAINED_MODELS_PATH)
 
 if not Path(MODEL_DATA).is_dir():
     os.mkdir(MODEL_DATA)
+
+if not Path(RESULTS).is_dir():
+    os.mkdir(RESULTS)
--- time_series/predict.py
+++ time_series/predict.py
@@ -3,8 +3,8 @@ import os
 import pickle
 
 import pandas as pd
-
-from config import TRAINED_MODELS_PATH, DATA_FILE_TEST
+from pathlib import Path
+from config import TRAINED_MODELS_PATH, DATA_FILE_TEST, RESULTS
 
 daysForcast = 20
 daysPredict = 100
@@ -14,22 +14,23 @@ Test = pd.read_csv(DATA_FILE_TEST, parse_dates=["date"])
 Test = Test.set_index("date").to_period("D")
 
 
-def forecast(res, days):
-    # days = 16
-    k = 2
+def forecast_model(res, days):
     pred_uc = res.get_forecast(steps=1 * days)
-    pred_ci = pred_uc.conf_int()
     return pred_uc.predicted_mean[4:]
 
 
 if __name__ == '__main__':
 
     how_many = int(sys.argv[1])
-    trained_models = []
+
+    print(f'how many samples: {how_many}')
+
     models_counter = 0
 
     for trained_model in os.listdir(TRAINED_MODELS_PATH):
         model_path = TRAINED_MODELS_PATH / trained_model
+
+        print(f'starting to predict model: {trained_model}')
         with open(model_path, 'rb') as f:
             model = pickle.load(f)
             trained_model_data = model['train_data']
@@ -38,7 +39,7 @@ if __name__ == '__main__':
         if models_counter > how_many:
             break
 
-        forecast = pd.DataFrame(forecast(trained_model_data, daysForcast))
+        forecast = pd.DataFrame(forecast_model(trained_model_data, daysForcast))
         forecast_df = pd.concat([forecast.transpose()], axis=1)
 
         idList = []
@@ -55,4 +56,6 @@ if __name__ == '__main__':
 
         ResultsDf = pd.DataFrame({'id': idList, 'sales': valueList})
         file_name = f'model_results_{model_detail_list[0]}_{model_detail_list[1]}'
-        ResultsDf.to_csv(file_name, sep='\t')
+        file_path = Path(RESULTS) / file_name
+        print(f'writing results to: {file_path}')
+        ResultsDf.to_csv(file_path, sep=',')
